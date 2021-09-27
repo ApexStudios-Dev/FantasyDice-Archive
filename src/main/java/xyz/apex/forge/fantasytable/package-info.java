149e338dfdffa2674a586e3ca491b3b30598d0e5
Robson Lawson <robsonlawson3@gmail.com>
2021-09-27 20:24:13 +0100

    Remove todo comment

--- src/main/java/xyz/apex/forge/fantasytable/package-info.java
+++ src/main/java/xyz/apex/forge/fantasytable/package-info.java
@@ -5,16 +5,3 @@ import mcp.MethodsReturnNonnullByDefault;
 import xyz.apex.forge.fantasytable.util.FieldsAreNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-/**
- * Finish migrating over to use registrate
- *
- * Tic-TacToe
- *  Block opens ui to play game in
- *
- * Playing Cards
- *  Three suits (Fire, Water, Grass)
- *  Each card has number
- *      Water beats Fire beats Grass
- *      If suits match highest number wins
- */
