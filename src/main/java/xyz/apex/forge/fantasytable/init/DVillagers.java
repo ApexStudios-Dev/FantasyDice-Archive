009740c3b8092bae9ecc00d7f226d9e6523905dc
Robson Lawson <robsonlawson3@gmail.com>
2021-09-28 22:44:13 +0100

    Add Trades for all dice levels and move to use BasicTrade() rather than custom implementation Fix not being able to spawn Gambler Villager type

--- src/main/java/xyz/apex/forge/fantasytable/init/DVillagers.java
+++ src/main/java/xyz/apex/forge/fantasytable/init/DVillagers.java
@@ -1,20 +1,12 @@
 package xyz.apex.forge.fantasytable.init;
 
 import com.tterrag.registrate.util.entry.RegistryEntry;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
-import net.minecraft.entity.merchant.villager.VillagerTrades;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.MerchantOffer;
-import net.minecraft.util.IItemProvider;
 import net.minecraft.village.PointOfInterestType;
+import net.minecraftforge.common.BasicTrade;
 import xyz.apex.forge.fantasytable.FantasyTable;
 import xyz.apex.forge.fantasytable.util.registrate.entry.VillagerProfessionEntry;
-
-import javax.annotation.Nullable;
-import java.util.Random;
 
 public final class DVillagers
 {
@@ -23,7 +15,7 @@ public final class DVillagers
 			.registrate()
 			.object(FStrings.POI_GAMBLER)
 			.pointOfInterestType()
-				.matchingBlock(FBlocks.GAMBLING_TABLE::get)
+				.matchingBlock(FBlocks.GAMBLING_TABLE)
 				.maxTickets(1)
 				.validRange(1)
 			.register();
@@ -32,22 +24,39 @@ public final class DVillagers
 			.registrate()
 			.object(FStrings.POI_GAMBLER)
 			.villagerProfession()
-				.villagerTrades((profession, trades) ->trades
-						.one(
-								new GenericTrade(() -> Items.IRON_INGOT, 1, Coins.IRON.item::get, 3),
-								new GenericTrade(() -> Items.GOLD_INGOT, 1, Coins.GOLD.item::get, 3)
+				.pointOfInterestType(GAMBLER_POI)
+				.villagerTrades((profession, trades) -> trades
+						// new BasicTrade(price1, price2, result, maxTrades, xp, priceMult)
+						// new BasicTrade(price1, result, maxTrades, xp, priceMult)
+						.one( // tier 1 trades
+								// ingot -> coin
+								new BasicTrade(Items.IRON_INGOT.getDefaultInstance(), Coins.IRON.item.asStack(), 20, 10, 1F),
+								new BasicTrade(Items.GOLD_INGOT.getDefaultInstance(), Coins.GOLD.item.asStack(), 20, 10, 1F),
+
+								// gold coin x 4 + iron ingot -> iron dice
+								new BasicTrade(Coins.GOLD.item.asStack(4), Items.IRON_INGOT.getDefaultInstance(), Dice.IRON.six_sided_die.asStack(), 10, 10, 1F),
+								new BasicTrade(Coins.GOLD.item.asStack(4), Items.IRON_INGOT.getDefaultInstance(), Dice.IRON.twenty_sided_die.asStack(), 10, 10, 1F)
 						)
-						.two(
-								new GenericTrade(Coins.GOLD.item::get, 8, () -> Items.IRON_INGOT, 1, Dice.IRON.six_sided_die::get, 1),
-								new GenericTrade(Coins.GOLD.item::get, 8, () -> Items.IRON_INGOT, 1, Dice.IRON.twenty_sided_die::get, 1),
-								new GenericTrade(Coins.GOLD.item::get, 12, () -> Items.GOLD_INGOT, 1, Dice.GOLD.six_sided_die::get, 1),
-								new GenericTrade(Coins.GOLD.item::get, 12, () -> Items.GOLD_INGOT, 1, Dice.GOLD.twenty_sided_die::get, 1)
+						.two( // tier 2 trades
+								// gold coin x 8 + gold ingot -> gold dice
+								new BasicTrade(Coins.GOLD.item.asStack(8), Items.GOLD_INGOT.getDefaultInstance(), Dice.GOLD.six_sided_die.asStack(), 10, 15, 1F),
+								new BasicTrade(Coins.GOLD.item.asStack(8), Items.GOLD_INGOT.getDefaultInstance(), Dice.GOLD.twenty_sided_die.asStack(), 10, 15, 1F)
 						)
-						.three(
-								new GenericTrade(Coins.GOLD.item::get, 20, () -> Items.DIAMOND, 1, Dice.DIAMOND.six_sided_die::get, 1),
-								new GenericTrade(Coins.GOLD.item::get, 20, () -> Items.DIAMOND, 1, Dice.DIAMOND.twenty_sided_die::get, 1),
-								new GenericTrade(Coins.GOLD.item::get, 24, () -> Items.EMERALD, 1, Dice.EMERALD.six_sided_die::get, 1),
-								new GenericTrade(Coins.GOLD.item::get, 24, () -> Items.EMERALD, 1, Dice.EMERALD.twenty_sided_die::get, 1)
+						.three( // tier 3 trades
+								// gold coin x 12 + diamond -> diamond dice
+								new BasicTrade(Coins.GOLD.item.asStack(12), Items.DIAMOND.getDefaultInstance(), Dice.DIAMOND.six_sided_die.asStack(), 10, 20, 1F),
+								new BasicTrade(Coins.GOLD.item.asStack(12), Items.DIAMOND.getDefaultInstance(), Dice.DIAMOND.twenty_sided_die.asStack(), 10, 20, 1F)
+						)
+						.four( // tier 4 trades
+								// gold coin x 16 + emerald -> emerald dice
+								new BasicTrade(Coins.GOLD.item.asStack(16), Items.EMERALD.getDefaultInstance(), Dice.EMERALD.six_sided_die.asStack(), 10, 25, 1F),
+								new BasicTrade(Coins.GOLD.item.asStack(16), Items.EMERALD.getDefaultInstance(), Dice.EMERALD.twenty_sided_die.asStack(), 10, 25, 1F)
+						)
+						.five( // tier 5 trades
+								// gold coin + ? -> loaded dice
+						)
+						.six( // tier 6 trades
+
 						)
 				)
 				.defaultLang()
@@ -56,52 +65,4 @@ public final class DVillagers
 
 	@Deprecated // internal use only
 	public static void register() { }
-
-	private static class GenericTrade implements VillagerTrades.ITrade
-	{
-		private final IItemProvider costA;
-		private final IItemProvider costB;
-		private final IItemProvider result;
-
-		private final int costACount;
-		private final int costBCount;
-		private final int resultCount;
-
-		public GenericTrade(IItemProvider costA, int costACount, IItemProvider costB, int costBCount, IItemProvider result, int resultCount)
-		{
-			this.costA = costA;
-			this.costB = costB;
-			this.result = result;
-
-			this.costACount = costACount;
-			this.costBCount = costBCount;
-			this.resultCount = resultCount;
-		}
-
-		public GenericTrade(IItemProvider cost, int costCount, IItemProvider result, int resultCount)
-		{
-			this(cost, costCount, () -> Items.AIR, 0, result, resultCount);
-		}
-
-		@Nullable
-		@Override
-		public MerchantOffer getOffer(Entity entity, Random rng)
-		{
-			// MerchantOffer(costA, costB, result, uses, maxUses, xp, priceMultiplier, demand)
-			return new MerchantOffer(itemOrEmpty(costA, costACount), itemOrEmpty(costB, costBCount), itemOrEmpty(result, resultCount), 10, 4, .2F);
-		}
-
-		private ItemStack itemOrEmpty(IItemProvider itemProvider, int count)
-		{
-			if(count <= 0)
-				return ItemStack.EMPTY;
-
-			Item item = itemProvider.asItem();
-
-			if(item == Items.AIR)
-				return ItemStack.EMPTY;
-
-			return new ItemStack(item, count);
-		}
-	}
 }
