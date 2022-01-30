184fb11b728e05e902cfbaf7b7c939b3d384d7d7
ApexModder <robsonlawson3@gmail.com>
2022-01-30 21:07:30 +0000

    Add speciality dice item tag

--- src/main/java/xyz/apex/forge/fantasydice/init/FTTags.java
+++ src/main/java/xyz/apex/forge/fantasydice/init/FTTags.java
@@ -5,6 +5,7 @@ import net.minecraft.tags.ITag;
 
 import xyz.apex.repack.com.tterrag.registrate.providers.ProviderType;
 
+@SuppressWarnings("unchecked")
 public final class FTTags
 {
 	private static final FTRegistry REGISTRY = FTRegistry.getRegistry();
@@ -17,11 +18,12 @@ public final class FTTags
 	public static final class Items
 	{
 		public static final ITag.INamedTag<Item> DICE = REGISTRY.itemTagModded("dice");
+		public static final ITag.INamedTag<Item> DICE_SPECIALTY = REGISTRY.itemTagModded("dice/specialty");
 
 		private static void bootstrap()
 		{
 			REGISTRY.addDataGenerator(ProviderType.ITEM_TAGS, provider -> {
-
+				provider.tag(DICE_SPECIALTY).addTags(FTDiceTypes.DICE_FANTASY.getTag(), FTDiceTypes.DICE_TOBI.getTag(), FTDiceTypes.DICE_APEX.getTag(), FTDiceTypes.DICE_SYMACON.getTag());
 			});
 		}
 	}
