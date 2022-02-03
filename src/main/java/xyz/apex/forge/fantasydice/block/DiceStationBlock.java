d155c61675fc6d17b4d34ba21a14c28bf70527eb
ApexModder <robsonlawson3@gmail.com>
2022-02-03 03:33:21 +0000

    Fix dice station not facing player when placed (from 1.18.1)

--- src/main/java/xyz/apex/forge/fantasydice/block/DiceStationBlock.java
+++ src/main/java/xyz/apex/forge/fantasydice/block/DiceStationBlock.java
@@ -6,9 +6,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.IWorldPosCallable;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.IFormattableTextComponent;
@@ -22,6 +24,8 @@ import javax.annotation.Nullable;
 
 public class DiceStationBlock extends Block
 {
+	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
 	public DiceStationBlock(Properties properties)
 	{
 		super(properties);
@@ -52,5 +56,30 @@ public class DiceStationBlock extends Block
 				(windowId, playerInventory, player) -> new DiceStationContainer(FTContainers.DICE_STATION.asContainerType(), windowId, playerInventory, IWorldPosCallable.create(level, pos)),
 				title
 		);
+	}
+
+	@Nullable
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext ctx)
+	{
+		return defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+	}
+
+	@Override
+	public BlockState rotate(BlockState blockState, Rotation rotation)
+	{
+		return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+	}
+
+	@Override
+	public BlockState mirror(BlockState blockState, Mirror mirror)
+	{
+		return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder)
+	{
+		builder.add(FACING);
 	}
 }
