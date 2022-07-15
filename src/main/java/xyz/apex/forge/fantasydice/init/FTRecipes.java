0056bf4e97f0674e603b7cc3446393f95dfddfaa
ApexModder <robsonlawson3@gmail.com>
2022-07-15 03:53:25 +0100

    Fix registering RecipeType at incorrect time

--- src/main/java/xyz/apex/forge/fantasydice/init/FTRecipes.java
+++ src/main/java/xyz/apex/forge/fantasydice/init/FTRecipes.java
@@ -5,9 +5,10 @@ import com.tterrag.registrate.util.entry.RegistryEntry;
 
 import net.minecraft.core.Registry;
 import net.minecraft.data.recipes.SingleItemRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.ItemLike;
-import net.minecraftforge.common.util.Lazy;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import xyz.apex.forge.apexcore.lib.item.crafting.SingleItemRecipe;
 import xyz.apex.forge.commonality.Mods;
@@ -15,12 +16,13 @@ import xyz.apex.forge.fantasydice.item.crafting.DiceStationRecipe;
 
 public final class FTRecipes
 {
-	public static final Lazy<RecipeType<DiceStationRecipe>> DICE_STATION_RECIPE_TYPE = Lazy.of(() -> RecipeType.register("%s:dice_station".formatted(Mods.FANTASY_DICE)));
-
 	public static final RegistryEntry<SingleItemRecipe.Serializer<DiceStationRecipe>> DICE_STATION_RECIPE = FTRegistry.INSTANCE
 			.object("dice_station")
-			.addRegisterCallback(Registry.RECIPE_SERIALIZER_REGISTRY, DICE_STATION_RECIPE_TYPE::get) // delay registration until correct time
 			.simple(Registry.RECIPE_SERIALIZER_REGISTRY, () -> new SingleItemRecipe.Serializer<>(DiceStationRecipe::new));
+
+	public static final RegistryEntry<RecipeType<DiceStationRecipe>> DICE_STATION_RECIPE_TYPE = FTRegistry.INSTANCE
+			.object("dice_station")
+			.simple(ForgeRegistries.Keys.RECIPE_TYPES, () -> RecipeType.simple(new ResourceLocation(Mods.FANTASY_DICE, "dice_station")));
 
 	static void bootstrap()
 	{
